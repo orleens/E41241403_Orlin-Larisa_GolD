547df2a615e788b8fbd1e6772635d06128d957fc
orleens <orlinlarisaj@gmail.com>
2025-02-07 15:45:46 +0700

    Create MingguSatuu.java

---
+++ MingguSatuu.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package minggusatuu;
+
+/**
+ *
+ * @author USER
+ */
+public class MingguSatuu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nama : Orlin Larisa Azaria");
+        System.out.println("Golongan : D");
+        System.out.println("NIM : E41241403");
+        System.out.println("Program Study : Teknik Informatika");
+       
+    }
+    
+}
